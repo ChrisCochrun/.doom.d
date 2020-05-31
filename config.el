cfa056c1113608cdd57537139207afe47e549a28
Chris Cochrun <chris@tfcconnection.org>
2020-05-31 13:46:02 -0500

    changed font to floating number

--- config.el
+++ config.el
@@ -22,7 +22,7 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
-(setq doom-font (font-spec :family "VictorMono Nerd Font" :size 13 :weight 'semi-bold)
+(setq doom-font (font-spec :family "VictorMono Nerd Font" :size 13.0 :weight 'semi-bold)
       doom-variable-pitch-font (font-spec :family "VictorMono Nerd Font" :size 14 :weight 'semi-bold))
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
